21ba013b0f8b867b6b3c03fda1f0f280329ae711
AntonStorozhenkoex <storozhenko.exceedteam@gmail.com>
2022-09-30 14:21:28 +0300

    Photogallery: added navigation styles

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,17 +1,31 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Link from "next/link";
 import styles from "../styles/Navigation.module.scss";
+import {useRouter} from "next/router";
 
-export const Navigation = () =>
-    <ul className={styles.list}>
+export const Navigation = () => {
+    const router = useRouter()
+    const [isHomeRoute, setIsHomeRoute] = useState(true)
+
+    useEffect(() => {
+        if (router.pathname === '/') {
+            setIsHomeRoute(true)
+        } else {
+            setIsHomeRoute(false)
+        }
+    }, [router])
+
+    return <ul className={styles.list}>
         <li className={styles.listItem}>
             <Link href="/">
-                <a className={styles.listItemLink}>Download</a>
+                <a className={styles.listItemLink} style={{textDecoration: isHomeRoute && 'underline'}}>Download</a>
             </Link>
         </li>
         <li className={styles.listItem}>
             <Link href="/photogallery">
-                <a className={styles.listItemLink}>Gallery</a>
+                <a className={styles.listItemLink} style={{textDecoration: isHomeRoute || 'underline'}}>Gallery</a>
             </Link>
         </li>
     </ul>
+}
+
